31d69374fbd36f1f5fc8d6e464f7cc9aac590795
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-02 14:47:34 +1100

    midleware - simple mw to play around

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ hbs.registerHelper('screamIt',(text)=>{
   return text.toUpperCase() + '!!';
 })
 
+//app.use is how we register a midleware and it takes a function
+app.use((req, res, next)=>{
+  //it's like getting in middle of the program and trying to run whatever is inside this.
+  //and it won't let the server proceed untill next() is called:
+  var now = new Date().toString();
+  //for further express methods:
+  //http://expressjs.com/en/4x/api.html#req
+  //http://expressjs.com/en/4x/api.html#res
+  console.log(`${now}: ${req.method} , ${req.url}`)
+  next()
+})
+
+
 app.get('/', (req, res)=> {
   res.render('home.hbs',{
     pageTitle: 'Home Page in HTML(Mustache)',
