1f615fc47fce3108294b44602568dd6424adc06f
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-02 13:41:05 +1100

    creating / with handlebars (hbs) render

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
-var express = require('express');
+const hbs = require('hbs') //handle bars
+const express = require('express');
+
 var app = express();
+
+app.set('view engine', 'hbs');
+
 
 //http://expressjs.com/en/4x/api.html#express.static
 var options = {
   extensions: ['htm', 'html']
 }
-
+//cool tool for any public page! no need to use get for each of the tabs. just put all in the specified directory.
+//try localhost:3000/help and localhost:3000/nothelp  :)
 app.use(express.static(__dirname + '/public',options))
 
 app.get('/', (req, res)=> {
-  // res.send('<p>Hello Express!</p>')
-  res.send({
-    name: 'Arsalan',
-    position: 'Entry Level Developer',
-    FavePlayers: [
-      'Messi',
-      'Suarez',
-      'Kroos',
-      'Ter Stegan',
-      'Jordi Alba',
-      {
-          sub: ['Coutiniho',
-                'Dembele',
-                'Mbappe',
-                'Nueuer',
-                'Arthor Melo'
-                ]
-      }
-    ]
+  res.render('home.hbs',{
+    pageTitle: 'Home Page in HTML(Mustache)',
+    currentYear: new Date().getFullYear(),
+    pContent: 'Just a Welcome msg'
   })
 })
 
 app.get('/about', (req, res)=>{
-  res.send('<h1>About Page</h1>')
+  res.render('about.hbs',{
+    pageTitle: 'About Page in HTML(Mustache)',
+    currentYear: new Date().getFullYear()
+  }) //'render' will render the current 'view engine'
 })
 
 app.get('/bad', (req, res)=>{
